5594ce346f0977f1fafc436ca18ffc456f184215
Zain Fathoni <zain.fathoni@gmail.com>
2020-12-31 21:56:32 +0800

    Use a Jest Mock Function

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,8 +7,7 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', () => {
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
   const username = 'zain'
   const password = 'fathoni'
@@ -17,8 +16,9 @@ test('submitting the form calls onSubmit with username and password', () => {
   userEvent.type(screen.getByLabelText(/password/i), password)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username,
     password
   })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
