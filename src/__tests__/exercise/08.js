be7d53f74abf613b6163cbf0554547c687a66485
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-02 01:34:11 +0800

    Fake Component

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,30 +2,19 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function UseCounterHookExample() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  render(<UseCounterHookExample />)
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-  const message = screen.getByText(/current count/i)
-
-  expect(message).toHaveTextContent('Current count: 0')
-  userEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  userEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+  let result
+  function TestComponent(props) {
+    result = useCounter()
+    return null
+  }
+  render(<TestComponent />)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
