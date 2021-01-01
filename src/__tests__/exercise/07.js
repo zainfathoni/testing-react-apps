33b17230476f9001c8dd6e0bdc9ad321faa638b6
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-02 00:40:43 +0800

    Dark Theme

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -17,3 +17,15 @@ test('renders with the light styles for the light theme', () => {
     color: black;
   `)
 })
+
+test('renders with the dark styles for the dark theme', () => {
+  function Wrapper({children}) {
+    return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+})
