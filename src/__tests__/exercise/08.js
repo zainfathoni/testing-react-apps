e7f0dfb11057776378d700894132d700994b4a7f
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-02 01:44:23 +0800

    Using React-Hooks Testing Library

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,22 +1,11 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react-hooks'
 import useCounter from '../../components/use-counter'
 
-function setup({initialProps} = {}) {
-  let result = {}
-  function TestComponent(props) {
-    result.current = useCounter(props)
-    return null
-  }
-  render(<TestComponent {...initialProps} />)
-  return result
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  const result = setup()
+  const {result} = renderHook(useCounter)
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
   expect(result.current.count).toBe(1)
@@ -25,15 +14,27 @@ test('exposes the count and increment/decrement functions', () => {
 })
 
 test('allows customization of the initial count', () => {
-  const result = setup({initialProps: {initialCount: 3}})
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 3}})
   expect(result.current.count).toBe(3)
 })
 
 test('allows customization of the step', () => {
-  const result = setup({initialProps: {step: 2}})
+  const {result} = renderHook(useCounter, {initialProps: {step: 2}})
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
   expect(result.current.count).toBe(2)
   act(() => result.current.decrement())
   expect(result.current.count).toBe(0)
+})
+
+test('the step can be changed', () => {
+  const {result, rerender} = renderHook(useCounter, {
+    initialProps: {step: 3},
+  })
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+  rerender({step: 2})
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(1)
 })
